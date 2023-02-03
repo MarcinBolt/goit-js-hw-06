46a61e8d6cc0ef543b936b66ccd9cb45b5184e08
MarcinBolt <marcin.boltruczyk@gmail.com>
2023-02-03 18:59:53 +0100

    solved task 02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,9 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const listIngredients = document.querySelector('#ingredients');
+
+for (const ingredient of ingredients) {
+  const addLiItem = document.createElement('li');
+  addLiItem.textContent = ingredient;
+  listIngredients.append(addLiItem);
+}
