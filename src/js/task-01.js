e0cf2ebb2538de51afd3f8a34ef7cd2088d87c00
MarcinBolt <marcin.boltruczyk@gmail.com>
2023-02-03 17:59:43 +0100

    changed approach to the task 1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,10 +1,22 @@
+// const listCategories = document.querySelector('#categories');
+// console.log(`Number of categories: ${listCategories.children.length}`);
+
+// const childrenOfListCategories = listCategories.children;
+
+// for (const listCategoriesChild of childrenOfListCategories) {
+//   console.log(`Category: ${listCategoriesChild.firstElementChild.textContent}`);
+//   console.log(`Elements: ${listCategoriesChild.lastElementChild.children.length}`);
+// }
+
 const listCategories = document.querySelector('#categories');
-console.log(`Number of categories: ${listCategories.children.length}`);
+const listCategoriesLength = listCategories.children.length;
+console.log(`Number of categories: ${listCategoriesLength}`);
 
-const childrenOfListCategories = listCategories.children;
+const liItemsOfListCategories = listCategories.querySelectorAll('li.item');
 
-for (const listCategoriesChild of childrenOfListCategories) {
-  console.log(`Category: ${listCategoriesChild.firstElementChild.textContent}`);
-  console.log(`Elements: ${listCategoriesChild.lastElementChild.children.length}`);
+for (const item of liItemsOfListCategories) {
+  const itemCategory = item.querySelector('h2');
+  console.log(`Category: ${itemCategory.textContent}`);
+  const itemList = item.querySelectorAll('li');
+  console.log(`Elements: ${itemList.length}`);
 }
-
