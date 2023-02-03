11cfc69c2d4fa05ed3943be82d04cb0704488e71
MarcinBolt <marcin.boltruczyk@gmail.com>
2023-02-03 19:07:21 +0100

    added class item to li elements

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -4,6 +4,7 @@ const listIngredients = document.querySelector('#ingredients');
 
 for (const ingredient of ingredients) {
   const addLiItem = document.createElement('li');
+  addLiItem.classList.add('item');
   addLiItem.textContent = ingredient;
   listIngredients.append(addLiItem);
 }
