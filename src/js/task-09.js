91350dcd481b32518d4ad09ce4741f706e1475ec
MarcinBolt <marcin.boltruczyk@gmail.com>
2023-02-07 23:55:13 +0100

    solved task 09

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const btnChangeColor = document.querySelector('.change-color');
+const spanChangeColor = document.querySelector('.color');
+const bodyTag = document.querySelector('body');
+
+btnChangeColor.addEventListener('click', event => {
+  let randomColor = getRandomHexColor();
+  spanChangeColor.textContent = randomColor;
+  bodyTag.style.backgroundColor = randomColor;
+});
