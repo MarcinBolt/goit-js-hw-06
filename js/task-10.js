f7c959fe9dcb06eeb0f81d998feb2a3f6b44b803
MarcinBolt <marcin.boltruczyk@gmail.com>
2023-02-12 21:42:52 +0100

    task 10 code simplification

--- js/task-10.js
+++ js/task-10.js
@@ -7,20 +7,20 @@ function createBoxes(amount) {
   let divHeight = 30;
 
   for (let i = 0; i < amount; i++) {
-    let divBGColor = getRandomHexColor();
-    let currentDiv = `<div style="background-color: ${divBGColor}; width: ${divWidth}px; height: ${divHeight}px;"></div>`;
+    const currentDiv = document.createElement('div');
+    currentDiv.style.backgroundColor = getRandomHexColor();
+    currentDiv.style.width = `${divWidth}px`;
+    currentDiv.style.height = `${divHeight}px`;
     tableOfDiv.push(currentDiv);
     divWidth += 10;
     divHeight += 10;
   }
-
-  const markup = tableOfDiv.map(element => `${element}`).join('');
-  divBoxes.insertAdjacentHTML('beforeend', markup);
+  divBoxes.append(...tableOfDiv);
 }
 
 function destroyBoxes() {
   divBoxes.remove();
-  divControl.insertAdjacentHTML('afterend', `<div id="boxes"></div>`);
+  divControl.insertAdjacentHTML('afterend', divIdBoxesEmptyTag);
   tableOfDiv = [];
   numberInput.value = '';
 
@@ -30,12 +30,13 @@ function destroyBoxes() {
 const numberInput = document.querySelector('input');
 const buttonCreate = document.querySelector('button[data-create]');
 const buttonDestroy = document.querySelector('button[data-destroy]');
+const divIdBoxesEmptyTag = `<div id="boxes"></div>`;
 let divBoxes = document.querySelector('div#boxes');
 const divControl = document.querySelector('div#controls');
 let tableOfDiv = [];
 
 buttonCreate.addEventListener('click', event => {
-  createBoxes(numberInput.value);
+  createBoxes(Number(numberInput.value));
 });
 buttonDestroy.addEventListener('click', event => {
   destroyBoxes();
